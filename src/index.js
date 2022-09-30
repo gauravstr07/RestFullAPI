53055657d5db5a378f43224f247c30cbf343be58
Gaurav Sutar <gauravstr05@gmail.com>
2022-09-30 13:34:02 +0530

    Create Student Using AsyncAwait

--- src/index.js
+++ src/index.js
@@ -8,17 +8,32 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 //Creating a new student
-app.post("/students", (req, res) => {
-  const user = new Student(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.send("Something went wrong on create student " + err);
-    });
+// app.post("/students", (req, res) => {
+//   const user = new Student(req.body);
+//   user
+//     .save()
+//     .then(() => {
+//       res.status(201).send(user);
+//     })
+//     .catch((err) => {
+//       res.send("Something went wrong on create student " + err);
+//     });
+// });
+
+
+//Adding student using AsyncAwait
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+    console.log(createUser);
+  } catch (error) {
+    res.send("Something went wrong on create student " + error);
+  }
 });
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello from expressjs server ");
